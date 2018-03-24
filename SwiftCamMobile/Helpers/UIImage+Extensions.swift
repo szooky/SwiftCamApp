86c8406ba3aec1ddd27a4b776d0fd318ca7b910b
Filip Szukała <filip.szukala@itmagination.com>
2018-03-24 21:47:39 +0100

    Refactored uiimage extensions.

---
+++ SwiftCamMobile/Helpers/UIImage+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  UIImage+Extensions.swift
+//  SwiftCamMobile
+//
+//  Created by Filip Szukala on 24/03/2018.
+//  Copyright © 2018 Filip Szukala. All rights reserved.
+//
+
+import UIKit
+
+enum CameraFilter: String {
+    case exposure = "CIExposureAdjust"
+    case blur = "CIGaussianBlur"
+}
+
+extension UIImageView {
+    private func apply(_ filterType: CameraFilter, withValue value: Any?, forKey key: String) {
+        guard let filter = CIFilter(name: filterType.rawValue) else { return }
+        guard let image = image else { return }
+
+        filter.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+        filter.setValue(value, forKey: key)
+
+        let ciContext  = CIContext(options: nil)
+        guard let result = filter.value(forKey: kCIOutputImageKey) as? CIImage else { return }
+
+        let boundingRect = CGRect(x: 0,
+                                  y: 0,
+                                  width: image.size.width,
+                                  height: image.size.height)
+
+        guard let cgImage = ciContext.createCGImage(result, from: boundingRect) else { return}
+        self.image = UIImage(cgImage: cgImage)
+    }
+
+    func exposure(withEV ev: CGFloat) {
+        apply(.exposure, withValue: ev, forKey: kCIInputEVKey)
+    }
+
+    func blur(withRadius radius: CGFloat) {
+        apply(.blur, withValue: radius, forKey: kCIInputRadiusKey)
+    }
+}
---
+++ SwiftCamMobile/Views/CameraScreenView.swift
@@ -0,0 +1,22 @@
+//
+//  CameraScreen\View.swift
+//  SwiftCamMobile
+//
+//  Created by Filip Szukala on 24/03/2018.
+//  Copyright © 2018 Filip Szukala. All rights reserved.
+//
+
+import UIKit
+
+class CameraScreenView: UIImageView {
+
+    
+
+    func change(apeture: String) {
+
+
+        
+    }
+
+
+}
